5a73dcdd6b0e815a28d3ca603db73b30c192ab5e
HenriqueSOA <henrique27122008@gmail.com>
2026-05-25 12:21:15 -0300

    projeto

---
+++ src/main/java/com/example/ojogo2/Ojogo2Application.java
@@ -0,0 +1,13 @@
+package com.example.ojogo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ojogo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ojogo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ojogo2/Ojogo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ojogo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ojogo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
